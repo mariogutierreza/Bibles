4553f7b98540a1a52cc1eb9a448b963fe1fed2b0
mariogutierreza <36140834+mariogutierreza@users.noreply.github.com>
2018-03-30 11:49:37 -0500

    Camara fix

--- Bibles/Bibles.Android/MainActivity.cs
+++ Bibles/Bibles.Android/MainActivity.cs
@@ -3,7 +3,9 @@
     using Android.App;
     using Android.Content.PM;
     using Android.OS;
+    using Android.Runtime;
     using FFImageLoading.Forms.Droid;
+    using Plugin.Permissions;
 
     [Activity(Label = "Bibles", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
@@ -18,6 +20,10 @@
             global::Xamarin.Forms.Forms.Init(this, bundle);
             CachedImageRenderer.Init(true);
             LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
